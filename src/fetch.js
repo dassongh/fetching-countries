e6dde114f38aac3af52d8fced5815a98a039679e
Oleksandr S <oleksandr.slv@gmail.com>
2021-10-19 17:22:59 +0300

    fixing fetch.js

--- src/fetch.js
+++ src/fetch.js
@@ -1,5 +1,4 @@
 export function fetchCountries(country) {
   return fetch(`https://restcountries.com/v3.1/name/${country}?fields=name,capital,population,flags,languages`)
-  .then(response => response.json())
-  .then(data => data);
+  .then(response => response.json());
 }
